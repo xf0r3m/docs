28f6a38a03524c8ff5c6afde5bf7cb6351f54798
xf0r3m <jakubstasinski@protonmail.com>
2022-05-27 10:56:38 +0200

    Dalszy rozwój vocex.sh

--- angielski/vocex.sh
+++ angielski/vocex.sh
@@ -3,12 +3,16 @@
 points=0;
 vocFile=/tmp/voc;
 userAnsFile=/tmp/userAns;
+ansFile=/tmp/ansFile;
 
 if [ -f $1 ]; then
   cp $1 /tmp/${1}.tmp;
   sourceFile=/tmp/${1}.tmp;
 	sourceFileNOL=$(wc -l $sourceFile | awk '{printf $1}');
   pointsToGet=$sourceFileNOL;
+  if [ -f $vocFile ]; then rm $vocFile; fi
+  if [ -f $userAnsFile ]; then rm $userAnsFile; fi
+  if [ -f $ansFile ]; then rm $ansFile; fi
   
   while [ $sourceFileNOL -gt 0 ]; do
     if [ $sourceFileNOL -eq 1 ]; then 
@@ -33,16 +37,20 @@ if [ -f $1 ]; then
   
     echo $voc >> $vocFile;
     echo $userAns >> $userAnsFile;
+    echo $answer >> $ansFile;
     sed -i "${randomLineNo}d" $sourceFile
     sourceFileNOL=$((sourceFileNOL - 1));
   done
 
   pointsPercentage=$(echo "print(${points}/${pointsToGet})" | python3);
-  if [ $(echo $pointsPrecentage | cut -c 1) -eq 1 ]; then
+  minMaxIndicator=$(echo $pointsPercentage | cut -c 1);
+  if [ $minMaxIndicator -eq 1 ]; then
     result=100;
+  elif [ $minMaxIndicator -eq 0 ]; then
+    result=0;
   else 
-    roundIndicator=$(echo "$pointPercentage" | cut -c 5);
-    result=$(echo "$pointPercentage" | cut -c 3-4);
+    roundIndicator=$(echo "$pointsPercentage" | cut -c 5);
+    result=$(echo "$pointsPercentage" | cut -c 3-4);
     if [ $roundIndicator -ge 5 ]; then result=$((result + 1)); fi
   fi
   if [ $result -ge 98 ] && [ $result -le 100 ]; then grad=6;
@@ -53,10 +61,18 @@ if [ -f $1 ]; then
   else grad=1;
   fi  
  
-  echo "${points}/${pointsToGet} = $result = $grad"; 
+  echo "${points}/${pointsToGet} = $result = $grad" | tee /tmp/lastresult; 
   rm $sourceFile;
-  rm $vocFile;
-  rm $userAnsFile;
+elif [ "$1" ] && [ "$1" = "--lastresult" ]; then
+  vocNOL=$(wc -l $vocFile | awk '{printf $1}');
+  i=1;
+  while [ $i -le $vocNOL ]; do
+    voc=$(sed -n "${i}p" $vocFile);
+    userAns=$(sed -n "${i}p" $userAnsFile);
+    answer=$(sed -n "${i}p" $ansFile); 
+    echo -e "$voc = \t\e[1;33m${userAns}\e[0m | \t\e[32m${answer}\e[0m";
+    i=$((i + 1));
+  done
 else
 	echo "Source file is needed to run a vocabulary exam";
 fi
