ba74667579ee3ea9d4552ea015c6b1c2f7c20f65
xf0r3m <jakubstasinski@protonmail.com>
2022-05-27 20:08:38 +0200

    Dalszy rozwój vocex.sh

--- angielski/vocex.sh
+++ angielski/vocex.sh
@@ -5,7 +5,7 @@ vocFile=/tmp/voc;
 userAnsFile=/tmp/userAns;
 ansFile=/tmp/ansFile;
 
-if [ -f $1 ]; then
+if [ -z $1 ]; then
   cp $1 /tmp/${1}.tmp;
   sourceFile=/tmp/${1}.tmp;
 	sourceFileNOL=$(wc -l $sourceFile | awk '{printf $1}');
@@ -34,6 +34,14 @@ if [ -f $1 ]; then
     answer=$(sed -n "${randomLineNo}p" $sourceFile | cut -d "=" -f $answerLang);
 
     if [ "$userAns" = "$answer" ]; then points=$((points + 1)); fi
+
+    if [ "$2" ] && [ "$2" = "--trainingmode" ]; then
+      if [ "$userAns" = "$answer" ]; then
+        echo -e "\e[1;32mCORRECT\e[0m ${voc} = ${userAns}";
+      else
+        echo -e "\e[1;31mNOT CORRECT\e[0m ${voc} = \e[1;33m${userAns}\e[0m | \e[1;32m${answer}\e[0m";
+      fi
+    fi
   
     echo $voc >> $vocFile;
     echo $userAns >> $userAnsFile;
@@ -43,26 +51,28 @@ if [ -f $1 ]; then
   done
 
   pointsPercentage=$(echo "print(${points}/${pointsToGet})" | python3);
-  minMaxIndicator=$(echo $pointsPercentage | cut -c 1);
-  if [ $minMaxIndicator -eq 1 ]; then
+  if [ $(echo $pointsPercentage | cut -c 1) -eq 1 ]; then
     result=100;
-  elif [ $minMaxIndicator -eq 0 ]; then
+  elif [ $(echo $pointsPercentage | cut -c 3) -eq 0 ]; then
     result=0;
   else 
     roundIndicator=$(echo "$pointsPercentage" | cut -c 5);
     result=$(echo "$pointsPercentage" | cut -c 3-4);
     if [ $roundIndicator -ge 5 ]; then result=$((result + 1)); fi
   fi
-  if [ $result -ge 98 ] && [ $result -le 100 ]; then grad=6;
-  elif [ $result -ge 86 ] && [ $result -le 97 ]; then grad=5;
-  elif [ $result -ge 66 ] && [ $result -le 85 ]; then grad=4;
-  elif [ $result -ge 50 ] && [ $result -le 65 ]; then grad=3;
-  elif [ $result -ge 30 ] && [ $result -le 49 ]; then grad=2;
-  else grad=1;
+
+  if [ $result -ge 98 ] && [ $result -le 100 ]; then grad=6; color="1;32";
+  elif [ $result -ge 86 ] && [ $result -le 97 ]; then grad=5; color="1;33";
+  elif [ $result -ge 66 ] && [ $result -le 85 ]; then grad=4; color="1;35";
+  elif [ $result -ge 50 ] && [ $result -le 65 ]; then grad=3; color="1;34";
+  elif [ $result -ge 30 ] && [ $result -le 49 ]; then grad=2; color="1;30";
+  else grad=1; color="1;31";
   fi  
- 
-  echo "${points}/${pointsToGet} = $result = $grad" | tee /tmp/lastresult; 
+  
+  echo -e "\e[${color}m${points}\e[0m/${pointsToGet} = \e[${color}m${result}\e[0m = \e[${color}m${grad}\e[0m" | tee /tmp/lastresult;
+  #echo "${points}/${pointsToGet}=${result}=${grad}" >> /tmp/lastresult; 
   rm $sourceFile;
+
 elif [ "$1" ] && [ "$1" = "--lastresult" ]; then
   vocNOL=$(wc -l $vocFile | awk '{printf $1}');
   i=1;
@@ -70,9 +80,16 @@ elif [ "$1" ] && [ "$1" = "--lastresult" ]; then
     voc=$(sed -n "${i}p" $vocFile);
     userAns=$(sed -n "${i}p" $userAnsFile);
     answer=$(sed -n "${i}p" $ansFile); 
-    echo -e "$voc = \t\e[1;33m${userAns}\e[0m | \t\e[32m${answer}\e[0m";
+    echo -e "$voc = \e[1;33m${userAns}\e[0m | \e[1;32m${answer}\e[0m";
     i=$((i + 1));
   done
+  echo -e "$(cat /tmp/lastresult)";
 else
+  echo "vocex.sh - vocabulary exam";
+  echo "morketsmerke.github.io @ 2022";
+  echo "Usage:"
+  echo "dictionary_file.txt - runs vocabulary exam based on dictionary file";
+  echo "dictionary_file.txt --trainingmode - runs learning mode (modified vocabulary exam)";
+  echo "--lastresult - print last exam with asked word, users answers, correct answers summary result";
 	echo "Source file is needed to run a vocabulary exam";
 fi
