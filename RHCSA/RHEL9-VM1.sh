8b90bea4c2edb272d9dfd2d17da45fbb1397b419
xf0r3m <jakubstasinski@protonmail.com>
2022-06-06 20:00:30 +0200

    Dodanie katalogu RHCSA oraz skrytów tworzących maszyny wirtualne

---
+++ RHCSA/RHEL9-VM1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+vboxmanage=$(which vboxmanage);
+vmname="RHEL9-VM1";
+bridged_if="wlp1s0";
+
+#echo $vboxmanage;
+
+
+$vboxmanage createvm --name $vmname --ostype RedHat_64 --register;
+
+$vboxmanage createhd --filename "$HOME/VirtualBox VMs/${vmname}/OS.vdi" --size \
+1024000 --format VDI --variant Standard;
+$vboxmanage storagectl $vmname --name SATA0 --add sata;
+
+$vboxmanage modifyvm $vmname --memory 1024
+$vboxmanage modifyvm $vmname --nic1 bridged
+$vboxmanage modifyvm $vmname --bridgeadapter1 $bridged_if;
+
+$vboxmanage storageattach $vmname --storagectl SATA0 --port 0 --type hdd --medium \
+"$HOME/VirtualBox VMs/${vmname}/OS.vdi";
+$vboxmanage storageattach $vmname --storagectl SATA0 --port 1 --type dvddrive \
+--medium "$HOME/Pobrane/rhel-baseos-9.0-x86_64-dvd.iso";
+
---
+++ RHCSA/RHEL9-VM2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+vboxmanage=$(which vboxmanage);
+vmname="RHEL9-VM2";
+bridged_if="wlp1s0";
+
+#echo $vboxmanage;
+
+
+$vboxmanage createvm --name $vmname --ostype RedHat_64 --register;
+$vboxmanage createhd --filename "$HOME/VirtualBox VMs/${vmname}/OS.vdi" --size \
+1024000 --format VDI --variant Standard;
+
+$vboxmanage createhd --filename "$HOME/VirtualBox VMs/${vmname}/VDO.vdi" --size \
+4096 --format VDI --variant Standard;
+$vboxmanage createhd --filename "$HOME/VirtualBox VMs/${vmname}/Stratis.vdi" \
+--size 1024 --format VDI --variant Standard;
+
+$vboxmanage storagectl $vmname --name SATA0 --add sata;
+$vboxmanage modifyvm $vmname --memory 2048
+$vboxmanage modifyvm $vmname --nic1 bridged
+$vboxmanage modifyvm $vmname --bridgeadapter1 $bridged_if;
+
+$vboxmanage storageattach $vmname --storagectl SATA0 --port 0 --type hdd --medium \
+"$HOME/VirtualBox VMs/${vmname}/OS.vdi";
+
+i=1;
+while [ $i -le 4 ]; do
+  $vboxmanage createhd --filename "$HOME/VirtualBox VMs/${vmname}/LVM${i}.vdi" --size \
+  250 --format VDI --variant Standard;
+  $vboxmanage storageattach $vmname --storagectl SATA0 --port $i --type hdd --medium \
+  "$HOME/VirtualBox VMs/${vmname}/LVM${i}.vdi";
+  i=$((i + 1));
+done
+
+$vboxmanage storageattach $vmname --storagectl SATA0 --port 5 --type hdd --medium \
+"$HOME/VirtualBox VMs/${vmname}/VDO.vdi";
+$vboxmanage storageattach $vmname --storagectl SATA0 --port 6 --type hdd --medium \
+"$HOME/VirtualBox VMs/${vmname}/Stratis.vdi";
+
+$vboxmanage storageattach $vmname --storagectl SATA0 --port 7 --type dvddrive \
+--medium "$HOME/Pobrane/rhel-baseos-9.0-x86_64-dvd.iso";
+
